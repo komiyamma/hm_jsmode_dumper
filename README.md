e124d29994c8379cd1e5347cfd73062199615e3d
komiyamma <komiyama0629@gmail.com>
2022-08-24 00:30:55 +0900

    コミット

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 秀丸マクロ jsmode 用の「JavaScript変数の内容を文字列にする」関数 stringify
 
-![stringify v1.0.1](https://img.shields.io/badge/stringify-v1.0.1-6479ff.svg)
+![stringify v1.0.3](https://img.shields.io/badge/stringify-v1.0.3-6479ff.svg)
 [![MIT](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
 ![Hidemaru 9.18](https://img.shields.io/badge/Hidemaru-v9.18-6479ff.svg)
 ![JScript](https://img.shields.io/badge/JScript-OK-6479ff.svg)
--- build/stringify.js
+++ build/stringify.js
@@ -2,125 +2,57 @@
  * Copyright (C) 2022 Akitsugu Komiyama
  * under the MIT License
  *
- * stringify v1.0.2
- *
- * - stringify関数は、JavaScriptの各種オブジェクトや関数等を文字列化したものを取得する関数です。
- * - stringify.dir関数は、stringifyしたものをconsole.logする関数です。debuginfo(2)でアウトプット枠にも表示されます。
- * PHPのvar_dumpなどに近いでしょう。
- * JSON.stringify とは異なるため注意してください。
+ * stringify v1.0.3
  */
 (function () {
     var guid = "{2A86CB06-3ABC-4EFE-A75A-3B028D1B4D72}";
-    var _outputpane_dllobj = null;
-    function _output(msg) {
-        if (!_outputpane_dllobj) {
-            _outputpane_dllobj = hidemaru.loadDll(hidemaruGlobal.hidemarudir() + "\\HmOutputPane.dll");
+    var op_dllobj = null;
+    function output(msg) {
+        if (!op_dllobj) {
+            op_dllobj = hidemaru.loadDll(hidemaruGlobal.hidemarudir() + "\\HmOutputPane.dll");
         }
-        if (_outputpane_dllobj) {
+        if (op_dllobj) {
             var msg_replaced = msg.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
-            return _outputpane_dllobj.dllFunc.Output(hidemaruGlobal.hidemaruhandle(0), msg_replaced);
+            return op_dllobj.dllFunc.Output(hidemaruGlobal.hidemaruhandle(0), msg_replaced);
         }
         return false;
     }
-    function __stringify_helper__(obj, level, space) {
-        var dumped_text = "";
-        if (!level)
-            level = 0;
-        var level_padding = "";
-        for (var j = 0; j < level + 1; j++) {
-            if (typeof (space) == "number") {
-                for (var s = 0; s < space; s++) {
-                    level_padding += " ";
-                }
-            }
-            else if (typeof (space) == "string") {
-                level_padding += space;
-            }
+    // 関数の時に、文字列に治す
+    function replacer(key, val) {
+        if (typeof val === "function") {
+            return val.toString();
         }
-        if (level >= 10) {
-            return level_padding + "..."; // スタックが深すぎる
-        }
-        if (typeof (obj) == 'object') {
-            for (var item in obj) {
-                var value = obj[item];
-                if (value === null) {
-                    dumped_text += level_padding + "\"" + item + "\" : (null),\n";
-                }
-                else if (typeof (value) == 'object') {
-                    dumped_text += level_padding + "\"" + item + "\" :\n";
-                    var next_level_text = __stringify_helper__(value, level + 1);
-                    if (next_level_text.length == 0) {
-                        dumped_text += level_padding + "{},";
-                    }
-                    else {
-                        dumped_text += level_padding + "{\n" + next_level_text + "\n" + level_padding + "},";
-                    }
-                }
-                else if (typeof (value) == "string") {
-                    dumped_text += level_padding + "\"" + item + "\" : \"" + value + "\",\n";
-                }
-                else if (typeof (value) == "function") {
-                    value = value.toString().replace(/[\n|\r]/g, " ");
-                    dumped_text += level_padding + "\"" + item + "\" : " + value + ",\n";
-                }
-                else {
-                    dumped_text += level_padding + "\"" + item + "\" : " + value + ",\n";
-                }
-            }
-        }
-        else {
-            dumped_text = "" + obj;
-        }
-        return dumped_text;
+        return val;
     }
-    function __stringify__(obj, space) {
+    function _stringify(obj, space) {
         if (space === void 0) {
             space = 2;
         }
         var dumped_text = "";
-        if (obj == null || obj == undefined) {
-            if (typeof (obj) == "undefined") { // typeofで判定する
-                dumped_text = "(undefined)";
-            }
-            else {
-                dumped_text = "(null)";
-            }
+        if (typeof (obj) == "undefined") { // typeofで判定する
+            return undefined;
         }
-        else if (typeof (obj) == "object") {
-            dumped_text = __stringify_helper__(obj, 0, space);
-            if (dumped_text.length == 0) {
-                dumped_text = "{}";
-            }
-            else {
-                dumped_text = "{\n" + dumped_text + "}";
-            }
-        }
-        else if (typeof (obj) == "function") {
-            dumped_text = obj.toString();
-        }
-        else {
-            dumped_text = JSON.stringify(obj);
-        }
+        var dumped_text = JSON.stringify(obj, replacer, space);
         if (dumped_text) {
             dumped_text = dumped_text.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
         }
         return "[type: " + typeof (obj) + "]\r\n" + dumped_text + "\r\n";
     }
-    function __dir__(obj, space) {
-        return console.log(__stringify__(obj, space));
+    function _dir(obj, space) {
+        return console.log(_stringify(obj, space));
     }
     if (typeof (module) != 'undefined' && module.exports) {
-        module.exports.stringify = __stringify__;
-        module.exports.dir = __dir__;
+        module.exports.stringify = _stringify;
+        module.exports.dir = _dir;
     }
     else {
         if (typeof (stringify) != 'undefined') {
             if (stringify.guid == null || stringify.guid != guid) {
-                _output("本モジュールとは異なるstringifyが、すでに定義されています。\r\n上書きします。\r\n");
+                output("本モジュールとは異なるstringifyが、すでに定義されています。\r\n上書きします。\r\n");
             }
         }
-        stringify = __stringify__;
-        stringify.dir = __dir__;
+        stringify = _stringify;
+        stringify.dir = _dir;
         stringify.guid = guid;
     }
 })();
