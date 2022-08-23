caf386119b65bcebc8740ff2b7851744550d7ebc
komiyamma <komiyama0629@gmail.com>
2022-08-24 01:01:54 +0900

    stringify ではなく dumper とした

--- src/dumper.ts
+++ src/dumper.ts
@@ -4,53 +4,69 @@
  *
  * stringify v1.0.3
  */
+
+declare var module: any
+declare var stringify: any;
+declare var console: any;
+declare var Dumper: { stringify: any, dir: any };
+
 (function () {
     var guid = "{2A86CB06-3ABC-4EFE-A75A-3B028D1B4D72}";
-    var op_dllobj = null;
-    function output(msg) {
+    var op_dllobj: hidemaru.ILoadDllResult = null;
+
+    function output(msg: string): boolean {
+
         if (!op_dllobj) {
             op_dllobj = hidemaru.loadDll(hidemaruGlobal.hidemarudir() + "\\HmOutputPane.dll");
         }
+
         if (op_dllobj) {
             var msg_replaced = msg.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
             return op_dllobj.dllFunc.Output(hidemaruGlobal.hidemaruhandle(0), msg_replaced);
         }
+
         return false;
     }
+
     // 関数の時に、文字列に治す
-    function replacer(key, val) {
-        if (typeof val === "function") {
-            return val.toString();
+    function replacer(key: string, value: any) {
+        if (typeof value === "function") {
+            return value.toString();
         }
-        return val;
+        return value;
     }
-    function _stringify(obj, space) {
-        if (space === void 0) { space = 2; }
-        var text = "";
+
+    function _stringify(obj: any, space: number | string = 2): string | undefined {
+        var text: string = "";
         if (typeof (obj) == "undefined") { // typeofで判定する
             return undefined;
         }
         var text = JSON.stringify(obj, replacer, space);
+
         if (text) {
             text = text.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
         }
+
         return "[type: " + typeof (obj) + "]\r\n" + text + "\r\n";
     }
-    function _dir(obj, space) {
+
+    function _dir(obj: any, space: number | string) {
         return console.log(_stringify(obj, space));
     }
+
     if (typeof (module) != 'undefined' && module.exports) {
         module.exports.stringify = _stringify;
         module.exports.dir = _dir;
-    }
-    else {
-        if (typeof (stringify) != 'undefined') {
-            if (stringify.guid == null || stringify.guid != guid) {
-                output("本モジュールとは異なるstringifyが、すでに定義されています。\r\n上書きします。\r\n");
+    } else {
+        if (typeof (Dumper) != 'undefined') {
+            if (Dumper.guid == null || Dumper.guid != guid) {
+                output("本モジュールとは異なるDumperが、すでに定義されています。\r\n上書きします。\r\n");
             }
         }
-        stringify = _stringify;
-        stringify.dir = _dir;
-        stringify.guid = guid;
+
+        Dumper = {
+            stringify: _stringify,
+            dir: _dir
+        };
     }
 })();
---
+++ types/dumper.ts
@@ -0,0 +1,56 @@
+/**
+ * @file 秀丸のjsmode用のTypeScript定義ファイル
+ * @author Akitsugu Komiyama
+ * @license MIT
+ * @version v1.0.2
+ */
+
+declare namespace Dump {
+    /**
+     *
+     * stringify関数は、JavaScriptの各種オブジェクトや関数等を文字列化したものを取得する関数です。    
+     * PHPのvar_dumpやJavaScriptのconsole.dirに近いものです。     
+     * 
+     * JSON.stringify とは異なるため注意してください。     
+     *
+     * @param obj
+     * 文字列化したいオブジェクトや変数などを指定する
+     * 
+     * @param space
+     * インデントのスペース数。     
+     * 文字列で指定した場合は、深度分が該当の文字列でインデントされる。
+     * 
+     * @see jsmode の stringify 関数
+     * @link https://xn--pckzexbx21r8q9b.net/?page=nobu_tool_hm_jsmode_stringify
+     * 
+     * @see JSON.stringify
+     * @link https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+     *
+     * @returns
+     * 指定のオブジェクトや値を文字列化したもの
+     */
+    declare function stringify(obj: any, space: number|string = 2): string;
+
+
+/**
+ *
+ * stringify 関数で文字列化したものを、console.log へと出力します。
+ * 
+ * @param obj
+ * 文字列化したいオブジェクトや変数などを指定する
+ * 
+ * @param space
+ * インデントのスペース数。     
+ * 文字列で指定した場合は、深度分が該当の文字列でインデントされる。
+ * 
+ * @see jsmode の stringify 関数
+ * @link https://xn--pckzexbx21r8q9b.net/?page=nobu_tool_hm_jsmode_stringify
+ * 
+ * @see JSON.stringify
+ * @link https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+ *
+ * @returns
+ * なし
+ */
+    function dir(obj: any, space: number|string = 2): void;
+}
